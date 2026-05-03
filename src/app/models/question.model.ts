5cb7c77219a9cd7408f55ea3e57796a925cc0864
arnaudetitia <arnaud.etitia@gmail.com>
2026-05-03 13:07:39 +0200

    evolution model question

---
+++ src/app/models/question.model.ts
@@ -0,0 +1,10 @@
+import { TriTypeEnum } from './tri.enum';
+
+export interface Question {
+  question: string;
+  bonneReponse: string;
+  mauvaisesReponses: string;
+  musique: string;
+  tri: TriTypeEnum;
+  joueeApresQuestion: boolean;
+}
