d821e3545a5ca2dceb0095b4b046e8d2daa62aac
arnaudetitia <arnaud.etitia@gmail.com>
2026-05-23 16:56:04 +0200

    nouveau lobby

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { LobbyDialogComponent } from './lobby-dialog/lobby-dialog.component';
+import { Router } from '@angular/router';
+import { JoueursStore } from '../../store/joueurs.store';
 
 @Component({
   selector: 'app-homepage.component',
@@ -8,7 +12,24 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './homepage.component.scss',
 })
 export class HomepageComponent {
+  lancerPartieDialog = inject(MatDialog);
+
+  constructor(
+    private router: Router,
+    private joueursStore: JoueursStore,
+  ) {}
+
   openLobby() {
-    console.log('Opening lobby...');
+    const dialogRef = this.lancerPartieDialog.open(LobbyDialogComponent, {
+      width: '50vw',
+      disableClose: true,
+    });
+
+    dialogRef.afterClosed().subscribe((joueurs) => {
+      if (joueurs) {
+        this.joueursStore.setJoueursQualifs(joueurs);
+        this.router.navigate(['/qualifs']);
+      }
+    });
   }
 }
